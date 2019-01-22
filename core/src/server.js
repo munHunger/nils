35dba58c122b44845f5b0e451e00639c1cccf172
munHunger <marcusmunger@hotmail.com>
2019-01-22 20:28:24 +0100

    Added logger and static link to solver

--- core/src/server.js
+++ core/src/server.js
@@ -1,15 +1,26 @@
+const chalk = require("chalk");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const logger = require("./log/logger");
+
+const ikSolver = require("./ik/ik-solver");
 
 const port = 8000;
 app.get("/", function(req, res) {
-  res.status(200).json({ status: "it's kay" });
+  logger.info("solving demo IK");
+  res.status(200).json(
+    ikSolver.solveIK({
+      start: { x: 0, y: 0 },
+      end: { x: 5, y: 0 },
+      jointLengths: [5, 5]
+    })
+  );
   res.end();
 });
 app.use(bodyParser.json({ type: "application/json" }));
 app.listen(port, () => {
-  console.log("We are live on " + port);
+  logger.info("NILS core is awake and listening on " + port);
 });
 
 module.exports = app;
