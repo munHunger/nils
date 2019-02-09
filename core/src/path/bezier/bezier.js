13f12fd4dab8032ea88a5392265ec6cbb1a55f5c
munHunger <marcusmunger@hotmail.com>
2019-02-09 13:01:51 +0100

    addedd jsDoc

--- core/src/path/bezier/bezier.js
+++ core/src/path/bezier/bezier.js
@@ -1,9 +1,24 @@
+/**
+ * Generate a list of points on the curve as defined by the points.
+ * @param {Array.<Array.<number>>} points a list of [x,y,z] coordinates that defines the curve. note that the curve only passes through the end points
+ * @param {number} steps the interpolation, or how many points to extract. the points will be uniformly distributed on the path
+ *
+ * @returns {Array.<Array.<number>>} a list on uniformly distributed points on the curve as defined by the points
+ */
 function curve(points, steps) {
   return Array.apply(null, { length: steps })
     .map((_, idx) => (1 / (steps - 1)) * idx)
     .map(s => step(points, s));
 }
 
+/**
+ * Calculate a point in the bezier curve as defined by the points
+ *
+ * @param {Array.<Array.<number>>} points a list of [x,y,z] coordinates that defines the curve. note that the curve only passes through the end points
+ * @param {number[]} s the progress of the curve. should be contained within 0 <= s <= 1
+ *
+ * @returns {number[]} a point on the bezier curve defined by the points that is s percent through the path
+ */
 function step(points, s) {
   let l = points
     .filter(p => p)
