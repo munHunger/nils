caa3fb7912542e0115ad628a23569101b3fa732a
munHunger <marcusmunger@hotmail.com>
2019-02-09 12:56:04 +0100

    fixed bezidr calculation issue

--- core/src/path/bezier/bezier.js
+++ core/src/path/bezier/bezier.js
@@ -1,30 +1,24 @@
-function curve(p1, p2, p3, p4, steps) {
-  return Array(steps)
-    .map((_, idx) => (1 / steps) * idx)
-    .map(s => step(p1, p2, p3, p4, s));
+function curve(points, steps) {
+  return Array.apply(null, { length: steps })
+    .map((_, idx) => (1 / (steps - 1)) * idx)
+    .map(s => step(points, s));
 }
 
-function step(p1, p2, p3, p4, step) {
-  const l1 = p1
-    .map((c, index) => c - p2[index])
-    .map((c, index) => c * step + p1[index]);
-  const l2 = p2
-    .map((c, index) => c - p3[index])
-    .map((c, index) => c * step + p2[index]);
-  const l3 = p3
-    .map((c, index) => c - p4[index])
-    .map((c, index) => c * step + p3[index]);
-
-  const r1 = l1
-    .map((c, index) => c - l2[index])
-    .map((c, index) => c * step + l1[index]);
-  const r2 = l2
-    .map((c, index) => c - l3[index])
-    .map((c, index) => c * step + l2[index]);
-
-  return r1
-    .map((c, index) => c - r2[index])
-    .map((c, index) => c * step + r1[index]);
+function step(points, s) {
+  let l = points
+    .filter(p => p)
+    .reduce((acc, val, index, arr) => {
+      if (index + 1 < arr.length)
+        acc.push(
+          val
+            .map((c, i) => c - arr[index + 1][i])
+            .map(c => c * s)
+            .map((c, i) => val[i] - c)
+        );
+      return acc;
+    }, []);
+  if (l.length === 1) return l[0];
+  return step(l, s);
 }
 
 module.exports = {
--- core/src/path/bezier/bezier.spec.js
+++ core/src/path/bezier/bezier.spec.js
@@ -2,22 +2,22 @@ const bezier = require("./bezier");
 
 describe("step", () => {
   it("start position is first point", () =>
-    expect(bezier.step([1, 3, 2], [5, 4, 0], [9, 5, 2], [1, 4, 4], 0)).toEqual([
-      1,
-      3,
-      2
-    ]));
+    expect(
+      bezier.step([[1, 3, 2], [5, 4, 0], [9, 5, 2], [1, 4, 4]], 0)
+    ).toEqual([1, 3, 2]));
   it("end position is last point", () =>
-    expect(bezier.step([1, 3, 2], [5, 4, 0], [9, 5, 2], [1, 4, 4], 1)).toEqual([
-      1,
-      4,
-      4
-    ]));
+    expect(
+      bezier.step([[1, 3, 2], [5, 4, 0], [9, 5, 2], [1, 4, 4]], 1)
+    ).toEqual([1, 4, 4]));
 });
 
 describe("path", () => {
   it("can solve a simple 2 step curve", () =>
-    expect(bezier.curve([1, 3, 2], [5, 4, 0], [9, 5, 2], [1, 4, 4], 2)).toEqual(
-      [[1, 3, 2], [1, 4, 4]]
-    ));
+    expect(
+      bezier.curve([[1, 3, 2], [5, 4, 0], [9, 5, 2], [1, 4, 4]], 2)
+    ).toEqual([[1, 3, 2], [1, 4, 4]]));
+  it("can solve a 42 step curve", () =>
+    expect(
+      bezier.curve([[1, 3, 2], [5, 4, 0], [9, 5, 2], [1, 4, 4]], 42).length
+    ).toEqual(42));
 });
